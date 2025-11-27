5ebcdb7b0b3cac7a3a41ac18dbb25311cf0d5ca6
Sara <saramorerasuarez@gmail.com>
2025-11-26 22:53:30 -0600

    Fix:Conversion ishalfday only unavaibility

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sitiowebb.Models;
 
 namespace Sitiowebb.Data
@@ -12,7 +13,21 @@ namespace Sitiowebb.Data
         }
 
         public DbSet<VacationRequest> VacationRequests => Set<VacationRequest>();
-
         public DbSet<Unavailability> Unavailabilities => Set<Unavailability>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Conversor bool <-> int SOLO para Unavailability.IsHalfDay
+            var boolToIntConverter = new ValueConverter<bool, int>(
+                v => v ? 1 : 0,
+                v => v == 1
+            );
+
+            builder.Entity<Unavailability>()
+                .Property(u => u.IsHalfDay)
+                .HasConversion(boolToIntConverter);
+        }
     }
 }
